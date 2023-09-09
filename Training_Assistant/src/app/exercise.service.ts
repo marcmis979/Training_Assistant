6ad123b3c91fbd76c26a180e58cb1e57487cbb05
RafHon <rafahon088@student.polsl.pl>
2023-09-08 18:22:23 -0700

    Front init

---
+++ Training_Assistant/src/app/exercise.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Exercise } from './exercise/model/exercise';
+import { ExerciseResponse } from './exercise/model/exercise-response';
+@Injectable({
+  providedIn: 'root'
+})
+export class ExerciseService {
+
+  private apiUrl = 'http://localhost:5014/ExerciseApi';
+
+  constructor(private http: HttpClient) { }
+
+  getExercise(id: number): Observable<Exercise> {
+    return this.http.get<Exercise>(`${this.apiUrl}/getExercise/${id}`);
+  }
+  addExercise(exercise: ExerciseResponse): Observable<Exercise> {
+    return this.http.post<Exercise>(`${this.apiUrl}/addExercise`, exercise);
+  }
+  updateExercise(id: number, updatedExercise: ExerciseResponse): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/updateExercise/${id}`, updatedExercise);
+  }
+  getBurnedPerHour(id:number): Observable<number>{
+    return this.http.get<number>(`${this.apiUrl}/burned/${id}`);
+  }
+  deleteExercise(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/deleteExercise/${id}`);
+  }
+}
---
+++ Training_Assistant/src/app/muscle-part/model/muscle-part.ts
@@ -0,0 +1,9 @@
+export class MusclePart{
+    id:number;
+    name:String;
+
+    constructor(id:number,name:String){
+        this.id=id;
+        this.name=name;
+    }
+}
