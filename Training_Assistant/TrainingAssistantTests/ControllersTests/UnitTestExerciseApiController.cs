96726cfb9100fe4e16a8f536ea7db085bc1343cc
matbachowski <72310751+matbachowski@users.noreply.github.com>
2023-09-13 21:57:01 +0200

    Tests implementation for all methods in ExerciceApiController

--- Training_Assistant/TrainingAssistantTests/ControllersTests/UnitTestExerciseApiController.cs
+++ Training_Assistant/TrainingAssistantTests/ControllersTests/UnitTestExerciseApiController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TraingAssistantDAL.Models;
+using TraingAssistantDAL.Repositories.Implementation;
 using TrainingAssistantBLL.BusinessLogic;
 using TrainingAssistantBOL.Controllers;
 using TrainingAssistantWebAPI.Controllers;
@@ -24,6 +26,94 @@ namespace TrainingAssistantTests.ControllersTests
 
             Assert.Equal(12000.0, exerciseApiController.burnedPerHour(1));
 
+        }
+        [Fact]
+        public void TestGetExercises()
+        {
+            Mock<IExerciseBs> mockExerciseBs = new Mock<IExerciseBs>();
+            List<Exercise> expectedExercises = new List<Exercise>
+            {
+                new Exercise { Id = 1, Name = "Exercise1" },
+                new Exercise { Id = 2, Name = "Exercise2" },
+            };
+            mockExerciseBs
+                .Setup(e => e.GetExercises())
+                .Returns(expectedExercises);
+
+            ExerciseApiController exerciseApiController = new ExerciseApiController(mockExerciseBs.Object);
+
+            List<Exercise> actualExercises = exerciseApiController.getExercises();
+
+            Assert.Equal(expectedExercises, actualExercises);
+        }
+
+        [Fact]
+        public void TestGetExerciseById()
+        {
+            Mock<IExerciseBs> mockExerciseBs = new Mock<IExerciseBs>();
+            Exercise expectedExercise = new Exercise { Id = 1, Name = "Exercise1" };
+            mockExerciseBs
+                .Setup(e => e.GetExerciseById(1))
+                .Returns(expectedExercise);
+
+            ExerciseApiController exerciseApiController = new ExerciseApiController(mockExerciseBs.Object);
+
+            Exercise actualExercise = exerciseApiController.getExercise(1);
+           
+            Assert.Equal(expectedExercise, actualExercise);
+        }
+
+        [Fact]
+        public void TestInsertExercise()
+        {
+            Mock<IExerciseBs> mockExerciseBs = new Mock<IExerciseBs>();
+            var exerciseToInsert = new Exercise { Id = 1, Name = "Exercise1" };
+            mockExerciseBs
+                .Setup(e => e.InsertExercise(exerciseToInsert))
+                .Verifiable();
+
+            ExerciseApiController exerciseApiController = new ExerciseApiController(mockExerciseBs.Object);
+
+            exerciseApiController.addExercise(exerciseToInsert);
+
+            mockExerciseBs.Verify(e => e.InsertExercise(exerciseToInsert), Times.Once);
+        }
+
+        [Fact]
+        public void TestUpdateExercise()
+        {
+            Mock<IExerciseBs> mockExerciseBs = new Mock<IExerciseBs>();
+            var exerciseToUpdate = new Exercise { Id = 1, Name = "Exercise1" };
+            mockExerciseBs
+                .Setup(e => e.UpdateExercise(exerciseToUpdate))
+                .Verifiable();
+            mockExerciseBs
+                .Setup(e => e.GetExerciseById(1))
+                .Returns(new Exercise { Id = 1, Name = "Exercise1" });
+
+            ExerciseApiController exerciseApiController = new ExerciseApiController(mockExerciseBs.Object);
+
+            exerciseApiController.UpdateExercise(1, exerciseToUpdate);
+
+            mockExerciseBs.Verify(e => e.UpdateExercise(exerciseToUpdate), Times.Once);
+        }
+
+        [Fact]
+        public void TestDeleteExercise()
+        {
+            Mock<IExerciseBs> mockExerciseBs = new Mock<IExerciseBs>();
+            mockExerciseBs
+                .Setup(e => e.DeleteExercise(1))
+                .Verifiable();
+            mockExerciseBs
+                .Setup(e => e.GetExerciseById(1))
+                .Returns(new Exercise { Id = 1, Name = "Exercise1" });
+
+            ExerciseApiController exerciseApiController = new ExerciseApiController(mockExerciseBs.Object);
+
+            exerciseApiController.DeleteExercise(1);
+
+            mockExerciseBs.Verify(e => e.DeleteExercise(1), Times.Once);
         }
     }
 }
